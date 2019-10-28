0eae261084e0861825718febf0de9ba5cf249aaf
don55odhiambo <dondejan55@gmail.com>
2019-10-28 11:26:51 +0300

    modified don.js

--- don.js
+++ don.js
@@ -14,33 +14,33 @@ function myform() {
     var mname = ["kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame",]
     var fname = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama",]
 
-    if(gender=="male" && wd=0) { return document.getElementById("result").innerHTML=mname[0];
+    if(gender=="male" && wd===0) { return document.getElementById("result").innerHTML=mname[0];
     }     
-    else if(gender=="male" && wd=1) { return document.getElementById("result").innerHTML=mname[1];
+    else if(gender=="male" && wd===1) { return document.getElementById("result").innerHTML=mname[1];
     }
-    else if(gender=="male" && wd=2) { return document.getElementById("result").innerHTML=mname[2];
+    else if(gender=="male" && wd===2) { return document.getElementById("result").innerHTML=mname[2];
     }
-    else if(gender=="male" && wd=3) { return document.getElementById("result").innerHTML=mname[3];
+    else if(gender=="male" && wd===3) { return document.getElementById("result").innerHTML=mname[3];
     }
-    else if(gender=="male" && wd=4) { return document.getElementById("result").innerHTML=mname[4];
+    else if(gender=="male" && wd===4) { return document.getElementById("result").innerHTML=mname[4];
     }
-    else if(gender=="male" && wd=5) { return document.getElementById("result").innerHTML=mname[5];
+    else if(gender=="male" && wd===5) { return document.getElementById("result").innerHTML=mname[5];
     }
-    else if(gender=="male" && wd=6) { return document.getElementById("result").innerHTML=mname[6];
+    else if(gender=="male" && wd===6) { return document.getElementById("result").innerHTML=mname[6];
     }
 
-    else if(gender=="female" && wd=0) { return document.getElementById("result").innerHTML=fname[0];
+    else if(gender=="female" && wd===0) { return document.getElementById("result").innerHTML=fname[0];
     }
-    else if(gender=="female" && wd=1) { return document.getElementById("result").innerHTML=fname[1];
+    else if(gender=="female" && wd===1) { return document.getElementById("result").innerHTML=fname[1];
     }
-    else if(gender=="female" && wd=2) { return document.getElementById("result").innerHTML=fname[2];
+    else if(gender=="female" && wd===2) { return document.getElementById("result").innerHTML=fname[2];
     }
-    else if(gender=="female" && wd=3) { return document.getElementById("result").innerHTML=fname[3];
+    else if(gender=="female" && wd===3) { return document.getElementById("result").innerHTML=fname[3];
     }
-    else if(gender=="female" && wd==4) { return document.getElementById("result").innerHTML=fname[4];
+    else if(gender=="female" && wd===4) { return document.getElementById("result").innerHTML=fname[4];
     }
-    else if(gender=="female" && wd==5) { return document.getElementById("result").innerHTML=fname[5];
+    else if(gender=="female" && wd===5) { return document.getElementById("result").innerHTML=fname[5];
     }
-    else if(gender=="female" && wd==6) { return document.getElementById("result").innerHTML=fname[6];
+    else if(gender=="female" && wd===6) { return document.getElementById("result").innerHTML=fname[6];
     }
     }
